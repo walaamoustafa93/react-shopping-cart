57e64ccaef955f6edf78998139546085a370a103
walaamoustafa93 <walaamoustafa93>
2022-11-15 08:24:09 +0200

    header comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,13 @@
+import Header from './components/Header/Header';
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="layout">
+      <Header />
+      <main>Content</main>
+      <footer>Footer</footer>
+    </div>
+  );
 }
 
 export default App;
